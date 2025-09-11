640ce147802fa5ba294aa331339fab36f9b3008f
Iron Man <pangdongcan@live.com>
2025-09-11 15:11:28 +0800

    feature-v2.0.0: feature encryptionTools is not being developed now.

---
+++ browser/pages/encryptionTools/encryptionTools.js
@@ -0,0 +1,17 @@
+/**
+ * In my license, all codes can be shared free of charge. 
+ * However, If my code is taken for commercial use, please maintain it yourself. 
+ * I am not obligated to take responsibility for your business application.
+ * Here is my email "pangdongcan@live.com"
+ * 
+ * Copyright © 2025 Micheal Pang. All rights reserved.
+ * 
+ * @file This file "encryptionTools.js" is part of project "my-mickarea-tool" , which is belong to Michael Pang (It's Me).
+ * @author  Micheal Pang (Dongcan Pang)
+ * @createDate  2025-09-11
+ * @version 1.0.0 
+ * @description  这是字符加密与数字签名功能
+ */
+"use strict"; // 这是严格模式下的 Javascript 代码
+
+import { documentReady } from "../../modules/myselfs/js/apis.js";
